b306d872177a679a137c53535e40f6021506b7ed
Colcolat <jj.zapatabuenfil@gmail.com>
2026-05-21 23:13:12 -0600

    chore: Domain - added repositories

---
+++ CatalogoApp.Domain/Interfaces/IItemReviewRepository.cs
@@ -0,0 +1,9 @@
+﻿using CatalogoApp.Domain.Models;
+
+namespace CatalogoApp.Domain.Interfaces;
+
+public interface IReviewRepository
+{
+    List<Review> ObtenerPorItem(int itemId);
+    void Agregar(Review review);
+}
---
+++ CatalogoApp.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using CatalogoApp.Domain.Models;
+
+namespace CatalogoApp.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    User? ObtenerPorUsername(string username);
+    void Agregar(User user);
+}
