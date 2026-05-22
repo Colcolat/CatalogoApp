5971de68c7bdd749a09e46acb3fc0633a46b40e7
Colcolat <jj.zapatabuenfil@gmail.com>
2026-05-21 23:06:41 -0600

    Chore: Domain - added Review.cs and User.cs

---
+++ CatalogoApp.Domain/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogoApp.Domain.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public int ItemId { get; set; }
+    public int Estrellas { get; set; }   // 1–5
+    public string Autor { get; set; } = string.Empty;
+    public string Comentario { get; set; } = string.Empty;
+}
